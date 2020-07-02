31572bffb8fee75570d3830dc74169d5e833ac7c
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-03 07:07:41 +0900

    Enumsを追加

---
+++ Assets/MyGame/Scripts/Global/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction8 {
+  LeftFront,  // 左前
+  Front,      // 前
+  RightFront, // 右前
+  Left,       // 左
+  Right,      // 右
+  LeftBack,   // 左後ろ
+  Back,       // 後ろ
+  RightBack,  // 右後ろ
+}
