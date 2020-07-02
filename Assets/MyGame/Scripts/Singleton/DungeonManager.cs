7f19fc740e8c1205625f2a40b1b27d45f2b3e057
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-03 07:12:47 +0900

    Dungeonにプレイヤーの配置をしてみる

--- Assets/MyGame/Scripts/Singleton/DungeonManager.cs
+++ Assets/MyGame/Scripts/Singleton/DungeonManager.cs
@@ -11,9 +11,46 @@ namespace Singleton {
     private Algorithm algorithm = new Algorithm();
     private Stage stage = new Stage();
 
+    Vector2Int pos;
+
+    private Vector2Int nextPos(Direction8 dir)
+    {
+      var p = this.pos;
+
+      switch(dir) {
+        case Direction8.Left: p.x -= 1; break;
+        case Direction8.Right: p.x += 1; break;
+        case Direction8.Front: p.y -= 1; break;
+        case Direction8.Back: p.y += 1; break;
+        default: break;
+      }
+
+      return p;
+    }
+
+    private void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        this.pos = this.stage.MovesPlayer(pos, this.nextPos(Direction8.Left));
+      }
+
+      if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        this.pos = this.stage.MovesPlayer(pos, this.nextPos(Direction8.Right));
+      }
+
+      if (Input.GetKeyDown(KeyCode.UpArrow)) { 
+        this.pos = this.stage.MovesPlayer(pos, this.nextPos(Direction8.Front));
+      }
+
+      if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        this.pos = this.stage.MovesPlayer(pos, this.nextPos(Direction8.Back));
+      }
+    }
+
     public void CreateStage()
     {
       this.algorithm.Make(this.stage, 3, 2, 0.5f);
+      this.pos = this.PlacesPlayer();
     }
 
     public Vector2Int PlacesPlayer()
