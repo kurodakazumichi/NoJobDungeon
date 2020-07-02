03086678fa1e0c4b4e2968fabee80a59af6aebf4
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-03 07:10:22 +0900

    SingletonManagerを追加

---
+++ Assets/MyGame/Scripts/Singleton/SingletonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Singleton {
+
+  /// <summary>
+  /// シングルトンを管理する
+  /// </summary>
+  public class SingletonManager : SingletonMonobehaviour<SingletonManager>
+  {
+    override protected void Awake()
+    {
+      base.Awake();
+      DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Setup(string name, GameObject parent)
+    {
+      var go = new GameObject(name);
+
+      switch(name) {
+        case "DungeonManager": go.AddComponent<DungeonManager>(); break;
+        default: break;
+      }
+      
+      go.transform.parent = parent.transform;
+    }
+  }
+}
