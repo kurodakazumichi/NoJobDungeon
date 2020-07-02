6ee140b521bc8955b711ecc7a0e4be47690cad9f
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-02 18:41:53 +0900

    シングルトンを追加

---
+++ Assets/MyGame/Scripts/Singleton/SingletonMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Singleton 
+{
+  public class SingletonMonobehaviour<T> : MonoBehaviour where T : MonoBehaviour
+  {
+    private static T instance;
+
+    public static T Instance
+    {
+      get {
+        if (instance == null) {
+          instance = (T)FindObjectOfType(typeof(T));
+          if (instance == null) {
+            Debug.LogError(typeof(T) + "がシーンに存在しません。");
+          }
+        }
+        return instance;
+      }
+    }
+
+    virtual protected void Awake()
+    {
+      if (this != Instance) {
+        Destroy(this);
+        return;
+      }
+      DontDestroyOnLoad(this.gameObject);
+    }
+  }
+}
