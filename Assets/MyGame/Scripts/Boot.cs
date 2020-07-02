f6fb5a442195ad79fe9d06c83f09cf7242a9fd15
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-02 18:59:42 +0900

    起動時に実行されるBootポイントを作成

---
+++ Assets/MyGame/Scripts/Boot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boot {
+  [RuntimeInitializeOnLoadMethod]
+  static void Initialize()
+  {
+    var system = new GameObject("System");
+
+    var dungeon = new GameObject("DungeonManager", typeof(Singleton.DungeonManager));
+    dungeon.transform.parent = system.transform;
+  }
+
+}
