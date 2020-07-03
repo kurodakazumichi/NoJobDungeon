8fe97bfc272671ee1bfd1b3376d5696e82026533
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-03 16:03:16 +0900

    SpriteのSortingOrder定義を追加

---
+++ Assets/MyGame/Scripts/Global/SpriteSortingOrder.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// Spriteのソート順
+/// </summary>
+public static class SpriteSortingOrder {
+  public const int BackGround = 0;
+  public const int Trap = 1;
+  public const int Item = 2;
+  public const int Enemy = 3;
+  public const int Player = 4;
+}
