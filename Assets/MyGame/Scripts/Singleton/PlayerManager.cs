ead92add0048b4a56fda671396a2f48198b6522e
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-04 18:20:16 +0900

    リファクタリング

--- Assets/MyGame/Scripts/Singleton/PlayerManager.cs
+++ Assets/MyGame/Scripts/Singleton/PlayerManager.cs
@@ -6,9 +6,75 @@ using Dungeon;
 
 namespace Singleton {
 
+  /// <summary>
+  /// プレイヤーマネージャー
+  /// </summary>
   public class PlayerManager : SingletonMonobehaviour<PlayerManager>
   {
+    //-------------------------------------------------------------------------
+    // メンバ変数
+
+    /// <summary>
+    /// プレイヤーマップチップ
+    /// </summary>
     private PlayerChip player = null;
+
+    //-------------------------------------------------------------------------
+    // 主要なプロパティ
+
+    public IReadOnlyPlayerChip Player
+    {
+      get { return this.player; }
+    }
+
+    /// <summary>
+    /// プレイヤーのゲームオブジェクト
+    /// </summary>
+    public GameObject PlayerObject
+    {
+      get
+      {
+        if (this.player == null) return null;
+        return this.player.gameObject;
+      }
+    }
+
+    //-------------------------------------------------------------------------
+    // プレイヤー生成
+
+    /// <summary>
+    /// プレイヤーを作成し、指定された座標に置く
+    /// </summary>
+    public void CreatePlayer(Vector2Int coord)
+    {
+      if (!this.player)
+      {
+        this.player = MapChipFactory.Instance.CreatePlayerChip();
+      }
+
+      this.player.Coord = coord;
+      this.player.transform.position = DungeonManager.Instance.GetPositionFromCoord(coord);
+    }
+
+    //-------------------------------------------------------------------------
+    // 移動関連の処理
+
+    /// <summary>
+    /// 指定方向にプレイヤーが移動できるかチェックし、移動できる場合は移動する。
+    /// 移動できなかった場合はfalse、移動した場合はtureを返す。
+    /// </summary>
+    public bool CheckAndMovePlayer(Direction8 direction)
+    {
+      // Playerの移動に関わらず方向は更新する
+      SetPlayerDirection(direction);
+
+      // 移動できない
+      if (!ChecksPlayerMovable(direction)) return false;
+
+      // プレイヤーを動かす
+      MovePlayer(direction);
+      return true;
+    }
 
     /// <summary>
     /// 指定した方向にプレイヤーが動けるかを確認
@@ -33,20 +99,13 @@ namespace Singleton {
       DungeonManager.Instance.UpdatePlayerCoord(coord);
     }
 
+    /// <summary>
+    /// プレイヤーの方向をセットする
+    /// </summary>
+    /// <param name="direction"></param>
     public void SetPlayerDirection(Direction8 direction)
     {
       this.player.Direction = direction;
-    }
-
-
-    public void CreatePlayer(Vector2Int coord)
-    {
-      if (!this.player){ 
-        this.player = MapChipFactory.Instance.CreatePlayerChip();
-      } 
-
-      this.player.Coord = coord;
-      this.player.transform.position = DungeonManager.Instance.GetPositionFromCoord(coord);
     }
 
     public void SetPlayerPosition(Vector3 pos)
@@ -56,21 +115,7 @@ namespace Singleton {
       this.player.transform.position = pos;
     }
 
-    public IReadOnlyPlayerChip Player
-    {
-      get { return this.player; }
-    }
 
-    /// <summary>
-    /// プレイヤーのゲームオブジェクト
-    /// </summary>
-    public GameObject PlayerObject
-    {
-      get {
-        if (this.player == null) return null;
-        return this.player.gameObject;
-      }
-    }
   }
 
 }
