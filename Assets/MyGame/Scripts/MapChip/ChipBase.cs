12e22428f7ce3b7cf3d40b3940b9e69e24c49bcc
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-04 09:15:37 +0900

    ChipBaseクラスを追加

---
+++ Assets/MyGame/Scripts/MapChip/ChipBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapChip {
+  /// <summary>
+  /// マップチップのベースクラス
+  /// </summary>
+  public class ChipBase : StatefullMonoBehavior
+  {
+    protected SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// SpriteRendererをアタッチする
+    /// </summary>
+    protected virtual void Awake()
+    {
+      this.spriteRenderer = this.gameObject.AddComponent<SpriteRenderer>();
+    }
+  }
+}
