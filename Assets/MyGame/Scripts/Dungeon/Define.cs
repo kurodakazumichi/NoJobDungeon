eb1cc5ab8e1b0d60de269a776eeb0294a0d51514
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-05 22:48:22 +0900

    プレイヤーの移動秒数は定数化して一元管理する #7

--- Assets/MyGame/Scripts/Dungeon/Define.cs
+++ Assets/MyGame/Scripts/Dungeon/Define.cs
@@ -10,6 +10,11 @@ namespace Dungeon
 		public const int HEIGHT = 40;
 		public const int MIN_ROOM_SIZE = 3;
 
+    /// <summary>
+    /// １ターン当たりの秒数
+    /// </summary>
+    public const float SEC_PER_TURN = 0.15f;
+
     public static Vector3 CHIP_SCALE
     {
       get { return new Vector3(1, 1 ,1); }
