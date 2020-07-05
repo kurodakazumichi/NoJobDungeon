befbb406377fb7398542e45051b3d5b137348f10
momoko hirata <momoko.hirata.1109@gmail.com>
2020-07-05 23:41:30 +0900

    ResourceManager 見直し

--- Assets/MyGame/Scripts/Singleton/ResourceManager.cs
+++ Assets/MyGame/Scripts/Singleton/ResourceManager.cs
@@ -1,53 +1,146 @@
 ﻿using System;
 using System.Collections;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Singleton
 {
   public class ResourceManager : SingletonMonobehaviour<ResourceManager>
   {
-    private class ResouceCache
+
+    private class Loader
     {
-      public string Name
+      private AsyncOperationHandle<IList<UnityEngine.Object>> objectLoadHandle;
+      private AsyncOperationHandle<IList<Sprite>> spriteLoadHandle;
+
+      public bool IsLoading = false;
+
+      /// <summary>
+      /// 読み込み（非同期）
+      /// </summary>
+      public async Task<List<UnityEngine.Object>> Load(string label)
       {
-        get
+        if (IsLoading) return null;
+
+        IsLoading = true;
+        var list = new List<UnityEngine.Object>();
+
         {
-          if( this.Object == null ) return string.Empty;
-          return this.Object.name;
+          var handle = Addressables.LoadAssetsAsync<UnityEngine.Object>(label, null);
+          await handle.Task;
+          if (handle.Status == AsyncOperationStatus.Succeeded)
+          {
+            this.objectLoadHandle = handle;
+            list.AddRange(handle.Result);
+          }
+        }
+
+        // spriete読み込み.
+        // サブアセットのため、別ロード.
+        if (this.objectLoadHandle.IsValid())
+        {
+          var locationHandle = Addressables.LoadResourceLocationsAsync(label, typeof(Sprite));
+          var locations = await locationHandle.Task;
+          foreach (var location in locations)
+          {
+            var handle = Addressables.LoadAssetAsync<IList<Sprite>>(location.PrimaryKey);
+            await handle.Task;
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+              this.spriteLoadHandle = handle;
+              list.AddRange(handle.Result);
+            }
+          }
+          Addressables.Release(locationHandle);
+        }
+
+        IsLoading = false;
+        return list;
+      }
+
+      /// <summary>
+      /// 解放
+      /// </summary>
+      public void Unload()
+      {
+        if (this.spriteLoadHandle.IsValid())
+        {
+          Addressables.Release(this.spriteLoadHandle);
+        }
+
+        if (this.objectLoadHandle.IsValid())
+        {
+          Addressables.Release(this.objectLoadHandle);
         }
       }
 
-      public string MainAssetName
+    }
+
+    private class Asset
+    {
+      public string Label { get; private set; }
+      private Loader loader = new Loader();
+      private List<UnityEngine.Object> cache = new List<UnityEngine.Object>();
+
+      /// <summary>
+      /// 読み込み（非同期）
+      /// </summary>
+      public async Task LoadAsync(string label)
       {
-        get
-        {
-          if (this.Object == null) return string.Empty;
-          switch (this.Object)
-          {
-            case Sprite sprite: return sprite.texture.name;
-          }
-          return this.Object.name;
-        }
+        this.Label = label;
+        this.cache = await loader.Load(this.Label);
       }
 
-      public Type Type => Object.GetType();
-
-      public UnityEngine.Object Object;
-
-      public bool IsSubAsset
+      /// <summary>
+      /// 解放
+      /// </summary>
+      public void Unload()
       {
-        get
+        this.cache.Clear();
+        loader.Unload();
+        this.Label = string.Empty;
+      }
+
+      /// <summary>
+      /// 指定アセットの取得
+      /// </summary>
+      public T Find<T>(string name) where T : UnityEngine.Object
+      {
+        foreach (var obj in cache)
         {
-          if (this.Object == null) return false;
-          switch (this.Object)
+          if (obj.GetType() == typeof(T) && obj.name == name)
           {
-            case Sprite sprite: return true;
+            return obj as T;
           }
-          return false;
         }
 
+        return null;
+      }
+
+      /// <summary>
+      /// 指定アセットの全取得
+      /// </summary>
+      public List<T> FindAll<T>(string name) where T : UnityEngine.Object
+      {
+        var list = new List<T>();
+
+        bool isSprite = typeof(T) == typeof(Sprite);
+        foreach (var obj in cache)
+        {
+          if (obj.GetType() == typeof(T))
+          {
+            var assetName = ( isSprite ) ? ( obj as Sprite ).texture.name : obj.name;
+            if (assetName == name)
+            {
+              list.Add(obj as T);
+            }
+          }
+        }
+
+        return list;
       }
     }
 
@@ -58,7 +151,7 @@ namespace Singleton
     private string loadingLabel = string.Empty;
 
     /// <summary>リソースキャッシュ</summary>
-    private List<ResouceCache> caches = new List<ResouceCache>();
+    private List<Asset> assets = new List<Asset>();
 
     //// Start is called before the first frame update
     //void Start()
@@ -69,6 +162,7 @@ namespace Singleton
     //void Update()
     //{
     //}
+
 
     /// <summary>
     /// ロード中か
@@ -87,76 +181,71 @@ namespace Singleton
 
       var sw = new System.Diagnostics.Stopwatch();
 
+
       // 開始
-      Debug.Log($"load start");
+      Debug.Log($"load start {label}");
       sw.Start();
-      caches.Clear();
       loadingLabel = label;
 
-      // 読み込み 
+      // 読み込み
+      bool isLoaded = assets.Find( x => x.Label == label ) != null ;
+      if (isLoaded == false)
       {
-        var handle = Addressables.LoadAssetsAsync<UnityEngine.Object>(label, null);
-        await handle.Task;
-        CacheLoadResource( handle.Result );
+        var asset = new Asset();
+        await asset.LoadAsync(label);
+        assets.Add(asset);
       }
-
-      // spriete読み込み.
-      // サブアセットのため、別ロード.
-      var locations = await Addressables.LoadResourceLocationsAsync(label, typeof(Sprite)).Task;
-      foreach (var location in locations)
-      {
-        var handle = Addressables.LoadAssetAsync<IList<Sprite>>(location.PrimaryKey);
-        await handle.Task;
-        CacheLoadResource( handle.Result ); 
-      }
-
+    
       // 完了
       sw.Stop();
-      Debug.Log($"load complete({sw.ElapsedMilliseconds.ToString()}ms)");
+      Debug.Log($"load complete {sw.ElapsedMilliseconds.ToString()}ms");
       loadingLabel = string.Empty;
     }
 
     /// <summary>
-    /// 読み込んだリソースをキャッシュする
+    /// 解放
     /// </summary>
-    private void CacheLoadResource<T>( IList<T> objects ) where T : UnityEngine.Object
+    public void Unload(string label )
     {
-      foreach (var obj in objects)
+      var index = assets.FindIndex(x => x.Label == label);
+      if (index >= 0)
       {
-        Debug.Log($"load : {obj}");
-        var cache = new ResouceCache() { Object = obj };
-        caches.Add(cache);
+        var asset = assets[index];
+        asset.Unload();
+        assets.RemoveAt( index );
       }
     }
     
     /// <summary>
     /// リソースの取得
     /// </summary>
-    public T GetResource<T>(string name) where T : class
+    public T GetResource<T>(string name) where T : UnityEngine.Object
     {
-      var cache = caches.Find( x => 
-        x.Type.Equals(typeof(T)) 
-        && (x.Name == name)
-      );
-      return cache?.Object as T;
+      foreach (var asset in assets)
+      {
+        var resource = asset.Find<T>(name);
+        if (resource != null)
+        {
+          return resource;
+        }
+      }
+
+      return null;
     }
 
     /// <summary>
     /// リソースの取得
     /// </summary>
-    public T[] GetResources<T>(string name) where T : class
+    public T[] GetResources<T>(string name) where T : UnityEngine.Object
     {
-      var targets = this.caches.FindAll(x =>
-        x.Type.Equals(typeof(T)) 
-        && ( x.IsSubAsset ? x.MainAssetName == name : x.Name == name )
-      );
-
-      var array = new T[targets.Count];
-      for (int i = 0; i < targets.Count; i++)
+      var list = new List<T>();
+      foreach (var asset in assets)
       {
-        array[i] = targets[i].Object as T;
+        var resources = asset.FindAll<T>(name);
+        list.AddRange( resources ); 
       }
-      return array;
+
+      return list.ToArray();
     }
   }
 }
