92d210f97c529d582b2fabb6a92d599b293f880a
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-07 01:22:29 +0900

    新しいFieldChipクラス #25

---
+++ Assets/MyGame/Scripts/MapChip/FieldChip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyGame.Singleton;
+
+namespace MyGame {
+
+  /// <summary>
+  /// 床や壁などのフィールド系のチップ
+  /// </summary>
+  public class FieldChip : MapChipBase
+  {
+    override protected void Awake()
+    {
+      base.Awake();
+    }
+
+    protected override void Start()
+    {
+      this.spriteRenderer.sortingOrder = SpriteSortingOrder.BackGround;
+    }
+  }
+
+}
