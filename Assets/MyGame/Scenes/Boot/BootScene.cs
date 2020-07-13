e0eb687ed58e6c01e0ed86e0d616e310abe6f361
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-13 17:34:59 +0900

    #56 Bootがシーン上にあるスクリプトのAwakeの後に動くというクソ仕様だったので廃止してBootシーンを使う事にした

---
+++ Assets/MyGame/Scenes/Boot/BootScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MyGame
+{
+  /// <summary>
+  /// 一番最初に起動し、必要なシングルトンの生成を行う。
+  /// その他にもゲーム起動時に必ずやっておきたい処理があればこのシーンに実装する。
+  /// </summary>
+  public class BootScene : SceneBase
+  {
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+      base.Start();
+
+      var system = new GameObject("GlobalSystem");
+      // 親要素をDontDestoryしておかないと、DontDestoryされた子であっても消される
+      // このDontDestoryOnLoadは大事だよ。
+      DontDestroyOnLoad(system);
+
+      // シングルトンを管理するシングルトン
+      var singleton = new GameObject("SingletonManager", typeof(SingletonManager));
+      singleton.transform.parent = system.transform;
+
+      // 常駐させたいシステムがあればここで生成
+      SingletonManager.Instance
+#if _DEBUG
+      .Setup(nameof(DebugMenuManager), system)
+#endif
+      .Setup(nameof(CameraManager), system)
+      .Setup(nameof(TimeManager), system)
+      .Setup(nameof(InputManager), system);
+
+      SceneManager.LoadScene("MyGame/Scenes/Dungeon/DungeonScene");
+    }
+  }
+}
