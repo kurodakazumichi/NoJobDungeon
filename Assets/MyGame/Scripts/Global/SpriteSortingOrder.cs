a4bcbfd465058b81a112a10d3bf1a86a48c5a778
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-13 16:33:19 +0900

    #55 GimmickChipを追加

--- Assets/MyGame/Scripts/Global/SpriteSortingOrder.cs
+++ Assets/MyGame/Scripts/Global/SpriteSortingOrder.cs
@@ -6,7 +6,7 @@
   public static class SpriteSortingOrder
   {
     public const int BackGround = 0;
-    public const int Trap = 1;
+    public const int Gimmick = 1;
     public const int Item = 2;
     public const int Enemy = 3;
     public const int Player = 4;
