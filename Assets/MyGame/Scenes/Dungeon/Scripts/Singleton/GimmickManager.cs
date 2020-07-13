b5709f000d1ef337b404bbf0f8e61f086d564f40
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-13 16:35:59 +0900

    #55 GimmickManagerを追加

---
+++ Assets/MyGame/Scenes/Dungeon/Scripts/Singleton/GimmickManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.Dungeon
+{
+  /// <summary>
+  /// ギミックマネージャー
+  /// </summary>
+  public class GimmickManager : SingletonMonobehaviour<GimmickManager>
+  {
+
+  }
+}
