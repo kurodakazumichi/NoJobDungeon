1ceade88366733312603c0aede7bdfc8ce1031ac
kazumichi kuroda <kazumichi96@gmail.com>
2020-07-16 01:35:38 +0900

    #91 CharBaseを追加

---
+++ Assets/MyGame/Scenes/Dungeon/Scripts/Unit/CharBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.Dungeon
+{
+  /// <summary>
+  /// キャラクターのベースクラス
+  /// </summary>
+  public abstract class CharBase
+  {
+    //-------------------------------------------------------------------------
+    // コンストラクタ
+    public CharBase()
+    {
+
+    }
+
+    //-------------------------------------------------------------------------
+    // Member
+
+    /// <summary>
+    /// キャラクターチップ
+    /// </summary>
+    protected CharChip chip;
+
+    //-------------------------------------------------------------------------
+    // Public Properity
+
+    /// <summary>
+    /// 座標
+    /// </summary>
+    public Vector2Int Coord { get; set; }
+
+  }
+}
